16894c3a534ecc5d34477325aff476382cc323ef
Brent Wice <bcwice@gmail.com>
2018-09-04 16:47:47 -0400

    enternig summary
    description

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass1 testClass1 = new TestClass1();
+            testClass1.Start();
+
+
+
+            Console.WriteLine($"complete...");
+            Console.WriteLine($"press any key to exit");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/TestClass1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class TestClass1
+    {
+        public TestClass1() {}
+
+        public void Start()
+        {
+            Console.WriteLine($"starting...");
+        }
+    }
+}
